60b3111ae61b5966398d9ab98fc0388c2e9bb25f
Steve Martinelli <stevemar@ca.ibm.com>
2015-09-11 00:18:46 -0500

    remove deprecation text for policy_dirs option
    this option was initially slated for removal, but the neutron
    folks are asking to keep the option so they can manage policies
    for different vendors and services. more information in the bug
    itself.
    
    Change-Id: I48dfeb0918e6a1833a65f7204556e094d661ad76
    Closes-Bug: #1428332

--- oslo_policy/opts.py
+++ oslo_policy/opts.py
@@ -34,7 +34,6 @@ _options = [
                help=_('Default rule. Enforced when a requested rule is not '
                       'found.'),
                deprecated_group='DEFAULT'),
-    # NOTE(stevemar): Remove this option in the M cycle, refer to bug 1428332
     cfg.MultiStrOpt('policy_dirs',
                     default=['policy.d'],
                     help=_('Directories where policy configuration files are '
@@ -44,8 +43,7 @@ _options = [
                            'policy_file must exist for these directories to '
                            'be searched.  Missing or empty directories are '
                            'ignored.'),
-                    deprecated_group='DEFAULT',
-                    deprecated_for_removal=True),
+                    deprecated_group='DEFAULT'),
 ]
 
 
