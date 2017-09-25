7e185ad96b043c0cb450a6dfc6245db7aa4a0fd6
Davanum Srinivas <davanum@gmail.com>
2017-09-25 09:25:53 -0400

    http/https check rules as stevedore extensions
    Why? HttpCheck/HttpsCheck are examples of rule checks
    that can be implemented outside of the oslo.policy
    library. Once we setup the infra for registering and
    using these as stevedore extensions, we automatically
    get the capability of other folks contributing to
    writing custom rules for their own use cases.
    
    * Add HttpCheck/HttpsCheck as entrypoints in setup.cfg
    * parser will check get_extensions() to see if there
      are any external checks registered
    * Move HttpCheck/HttpsCheck into external module
    * Move related test cases to test_external.py
    
    Change-Id: Icde2b26a38d7c7842defae053228d9208454b969

--- oslo_policy/_checks.py
+++ oslo_policy/_checks.py
@@ -17,19 +17,25 @@
 
 import abc
 import ast
-import contextlib
-import copy
 import inspect
-import os
 
-from oslo_serialization import jsonutils
-import requests
 import six
-
-from oslo_policy._i18n import _
-
+import stevedore
 
 registered_checks = {}
+extension_checks = None
+
+
+def get_extensions():
+    global extension_checks
+    if extension_checks is None:
+        em = stevedore.ExtensionManager('oslo.policy.rule_checks',
+                                        invoke_on_load=False)
+        extension_checks = {
+            extension.name: extension.plugin
+            for extension in em
+        }
+    return extension_checks
 
 
 def _check(rule, target, creds, enforcer, current_rule):
@@ -274,107 +280,6 @@ class RoleCheck(Check):
         if 'roles' in creds:
             return match.lower() in [x.lower() for x in creds['roles']]
         return False
-
-
-@register('http')
-class HttpCheck(Check):
-    """Check ``http:`` rules by calling to a remote server.
-
-    This example implementation simply verifies that the response
-    is exactly ``True``.
-    """
-
-    def __call__(self, target, creds, enforcer, current_rule=None):
-        url = ('http:' + self.match) % target
-
-        # Convert instances of object() in target temporarily to
-        # empty dict to avoid circular reference detection
-        # errors in jsonutils.dumps().
-        temp_target = copy.deepcopy(target)
-        for key in target.keys():
-            element = target.get(key)
-            if type(element) is object:
-                temp_target[key] = {}
-
-        data = json = None
-        if (enforcer.conf.oslo_policy.remote_content_type ==
-                'application/x-www-form-urlencoded'):
-            data = {'rule': jsonutils.dumps(current_rule),
-                    'target': jsonutils.dumps(temp_target),
-                    'credentials': jsonutils.dumps(creds)}
-        else:
-            json = {'rule': current_rule,
-                    'target': temp_target,
-                    'credentials': creds}
-
-        with contextlib.closing(requests.post(url, json=json, data=data)) as r:
-            return r.text.lstrip('"').rstrip('"') == 'True'
-
-
-@register('https')
-class HttpsCheck(Check):
-    """Check ``https:`` rules by calling to a remote server.
-
-    This example implementation simply verifies that the response
-    is exactly ``True``.
-    """
-
-    def __call__(self, target, creds, enforcer, current_rule=None):
-        url = ('https:' + self.match) % target
-
-        cert_file = enforcer.conf.oslo_policy.remote_ssl_client_crt_file
-        key_file = enforcer.conf.oslo_policy.remote_ssl_client_key_file
-        ca_crt_file = enforcer.conf.oslo_policy.remote_ssl_ca_crt_file
-        verify_server = enforcer.conf.oslo_policy.remote_ssl_verify_server_crt
-
-        if cert_file:
-            if not os.path.exists(cert_file):
-                raise RuntimeError(
-                    _("Unable to find ssl cert_file  : %s") % cert_file)
-            if not os.access(cert_file, os.R_OK):
-                raise RuntimeError(
-                    _("Unable to access ssl cert_file  : %s") % cert_file)
-        if key_file:
-            if not os.path.exists(key_file):
-                raise RuntimeError(
-                    _("Unable to find ssl key_file : %s") % key_file)
-            if not os.access(key_file, os.R_OK):
-                raise RuntimeError(
-                    _("Unable to access ssl key_file  : %s") % key_file)
-        cert = (cert_file, key_file)
-        if verify_server:
-            if ca_crt_file:
-                if not os.path.exists(ca_crt_file):
-                    raise RuntimeError(
-                        _("Unable to find ca cert_file  : %s") % ca_crt_file)
-                verify_server = ca_crt_file
-
-        # Convert instances of object() in target temporarily to
-        # empty dict to avoid circular reference detection
-        # errors in jsonutils.dumps().
-        temp_target = copy.deepcopy(target)
-        for key in target.keys():
-            element = target.get(key)
-            if type(element) is object:
-                temp_target[key] = {}
-
-        data = json = None
-        if (enforcer.conf.oslo_policy.remote_content_type ==
-                'application/x-www-form-urlencoded'):
-            data = {'rule': jsonutils.dumps(current_rule),
-                    'target': jsonutils.dumps(temp_target),
-                    'credentials': jsonutils.dumps(creds)}
-        else:
-            json = {'rule': current_rule,
-                    'target': temp_target,
-                    'credentials': creds}
-
-        with contextlib.closing(
-                requests.post(url, json=json,
-                              data=data, cert=cert,
-                              verify=verify_server)
-        ) as r:
-            return r.text.lstrip('"').rstrip('"') == 'True'
 
 
 @register(None)
